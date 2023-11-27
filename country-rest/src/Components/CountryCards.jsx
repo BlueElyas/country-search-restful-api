eb9e80fbca4f9de95e6a62d1be9216df31e17bb2
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-27 15:24:12 +0000

    change background in components

--- country-rest/src/Components/CountryCards.jsx
+++ country-rest/src/Components/CountryCards.jsx
@@ -5,7 +5,7 @@ import data from '../../data.json/'
 export default function CountryCards() {
     const formattedData = data.map(element => {
         return (
-                <div className="bg-gray-600 text-white flex flex-col justify-center rounded-lg mt-16 w-64 h-96">
+                <div className="bg-gray-700 text-white flex flex-col justify-center rounded-lg mt-16 w-64 h-96">
                     <img className=" rounded-t-lg w-64 h-1/2" src={element.flags.png} alt="" />
                     <div className="p-4 mb-4 h-1/2">
                         <h5 className="my-4 font-bold">{element.name}</h5>
--- country-rest/src/Components/Header.jsx
+++ country-rest/src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return(
         <>
-            <div className="bg-gray-600 text-white flex justify-between py-4 px-8">
+            <div className="bg-gray-700 text-white flex justify-between py-4 px-8">
                 <h1 className="font-bold text-xl">Where in the world?</h1>
                 <button>Dark Mode</button>
             </div>
