0b8d37f683f1057acc153c838ecf696d078d3216
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-27 14:43:28 +0000

    add formatted data that maps into jsx

--- country-rest/src/App.jsx
+++ country-rest/src/App.jsx
@@ -6,8 +6,10 @@ function App() {
   return (
     
     <>
+    <div className=" ">
       <Header/>
       <CountryCards/>
+      </div>
     </>
   )
 }
--- country-rest/src/Components/CountryCards.jsx
+++ country-rest/src/Components/CountryCards.jsx
@@ -3,8 +3,27 @@ import data from '../../data.json/'
 
 
 export default function CountryCards() {
+    const afghanistan = data.filter(d => d.name.toLowerCase() === 'afghanistan')
+    const formattedData = data.map((element, index) => {
+        return (
+                <div className="bg-gray-600 text-white flex flex-col justify-center rounded-lg mt-16 w-64">
+                    <img className=" rounded-t-lg w-64 " src={element.flags.png} alt="" />
+                    <div className="p-4 mb-4">
+                        <h5 className="my-4 font-bold">{element.name}</h5>
+                        <ul>
+                            <li>Population: <span className="text-gray-300"> {element.population}</span></li>
+                            <li>Region: <span className="text-gray-300"> {element.region}</span></li>
+                            <li>Capital: <span className="text-gray-300"> {element.capital}</span></li>
+                        </ul>
+                    </div>
+                </div>
+        )
+    })
     return(
         <>
+            <div className="bg-gray-800 flex flex-wrap  items-start justify-around">
+                {formattedData}           
+            </div>
         </>
     )
 }
