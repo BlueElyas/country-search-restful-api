a52e85c7b11c4622790384d1a2c9cd0b95056876
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-27 17:52:31 +0000

    move mapped cards into their own component

--- country-rest/src/Components/Card.jsx
+++ country-rest/src/Components/Card.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
-export default function Card() {
-    return(
-        <>
-            <div className="">
-                Hello
-            </div>
-        </>
-    )
+export default function Card( {data} ) {
+    return  data.map((element, index) => {
+        return (
+                <div 
+                    className="bg-gray-700 text-white flex 
+                    flex-col justify-center rounded-lg mt-8 h-80 ml-16 cursor-pointer w-1/4" 
+                    key={index}      
+                >
+                    <img className=" rounded-t-lg w-64 h-1/2" src={element.flags.png} alt="" onClick={() => <Card/>}/>
+                    <div className="px-4  h-1/2 flex flex-col">
+                        <h5 className="my-4 font-bold ">{element.name}</h5>
+                        <ul>
+                            <li>Population: <span className="text-gray-300"> {element.population}</span></li>
+                            <li>Region: <span className="text-gray-300"> {element.region}</span></li>
+                            <li>Capital: <span className="text-gray-300"> {element.capital}</span></li>
+                        </ul>
+                    </div>
+                </div>
+        )
+    })
+
 }
--- country-rest/src/Components/CountryCards.jsx
+++ country-rest/src/Components/CountryCards.jsx
@@ -1,41 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import data from '../../data.json/'
 import Card from "./Card";
 
 
 export default function CountryCards() {
     const [searchElement, setSearchElement] = useState('')
-
-    const formattedData = data.map((element, index) => {
-        return (
-                <div 
-                    className="bg-gray-700 text-white flex 
-                    flex-col justify-center rounded-lg mt-8 h-80 ml-16 cursor-pointer w-1/4" 
-                    key={index}      
-                >
-                    <img className=" rounded-t-lg w-64 h-1/2" src={element.flags.png} alt="" onClick={() => <Card/>}/>
-                    <div className="px-4  h-1/2 flex flex-col">
-                        <h5 className="my-4 font-bold ">{element.name}</h5>
-                        <ul>
-                            <li>Population: <span className="text-gray-300"> {element.population}</span></li>
-                            <li>Region: <span className="text-gray-300"> {element.region}</span></li>
-                            <li>Capital: <span className="text-gray-300"> {element.capital}</span></li>
-                        </ul>
-                    </div>
-                </div>
-        )
-    })
-
     const handleChange = e => {
         setSearchElement(e.target.value)
     }
 
-    const filteredItems = data.filter(element => {
-        if (element.name.toLowerCase() === searchElement) element
-    })
-
     return(
         <>
+            
             <div className="bg-gray-800 px-24 py-8 ">
                 <input 
                     type="text" 
@@ -46,7 +22,7 @@ export default function CountryCards() {
                 />
             </div>
             <div className="bg-gray-800 flex flex-wrap px-8">  
-                {formattedData}           
+                {<Card data={data}/>}           
             </div>
         </>
     )
