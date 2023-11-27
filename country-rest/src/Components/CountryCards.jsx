8a53b9e9fcdadc6108a9381d38e1d1d630f3c2f2
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-27 17:37:21 +0000

    update withs for formatteddata in country cards component

--- country-rest/src/Components/CountryCards.jsx
+++ country-rest/src/Components/CountryCards.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import data from '../../data.json/'
+import Card from "./Card";
 
 
 export default function CountryCards() {
     const [searchElement, setSearchElement] = useState('')
+
     const formattedData = data.map((element, index) => {
         return (
-                <div className="bg-gray-700 text-white flex flex-col justify-center rounded-lg mt-8 w-autos h-80 ml-16 " key={index}>
-                    <img className=" rounded-t-lg w-64 h-1/2" src={element.flags.png} alt="" />
-                    <div className="p-4 h-1/2 flex flex-col">
+                <div 
+                    className="bg-gray-700 text-white flex 
+                    flex-col justify-center rounded-lg mt-8 h-80 ml-16 cursor-pointer w-1/4" 
+                    key={index}      
+                >
+                    <img className=" rounded-t-lg w-64 h-1/2" src={element.flags.png} alt="" onClick={() => <Card/>}/>
+                    <div className="px-4  h-1/2 flex flex-col">
                         <h5 className="my-4 font-bold ">{element.name}</h5>
                         <ul>
                             <li>Population: <span className="text-gray-300"> {element.population}</span></li>
@@ -23,12 +29,17 @@ export default function CountryCards() {
     const handleChange = e => {
         setSearchElement(e.target.value)
     }
+
+    const filteredItems = data.filter(element => {
+        if (element.name.toLowerCase() === searchElement) element
+    })
+
     return(
         <>
             <div className="bg-gray-800 px-24 py-8 ">
                 <input 
                     type="text" 
-                    className="bg-gray-700 rounded-sm py-2 px-8 w-1/2 placeholder:items-start" 
+                    className="bg-gray-700 rounded-sm py-2 px-8 w-1/2 placeholder:items-start text-gray-200" 
                     placeholder="Search for a country..." 
                     value={searchElement}
                     onChange={handleChange}
