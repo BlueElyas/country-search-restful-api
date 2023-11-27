cf3293a00899da7f2fa2f0878dcd5eff26d743d0
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-27 20:43:05 +0000

    fix structure in cards component

--- country-rest/src/Components/Card.jsx
+++ country-rest/src/Components/Card.jsx
@@ -1,22 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export default function Card( { data, searchInput } ) {
-    // const [search, setSearch] = useState(' ')
-
-    console.log(data.filter(e => {
-        if(e.type){
-            return e
-        }
-    }))
 
     const mappedElement = (newData) => newData.map((element, index) => {
         return (
                 <div 
                     className="bg-gray-700 text-white flex 
-                    flex-col justify-center rounded-lg mt-6  ml-16 cursor-pointer w-1/5" 
+                    flex-col justify-center rounded-lg mt-6  ml-16 cursor-pointer" 
                     key={index}      
                 >
-                    <img className=" rounded-t-lg w-full h-1/2" src={element.flags.png} onClick={() => <Card/>}/>
+                    <img className=" rounded-t-lg w-full h-1/2" src={element.flags.png}/>
                     <div className=" px-4 h-full flex flex-col py-2">
                         <h5 className="my-4 font-bold w-3/4">{element.name}</h5>
                         <ul className="w-full ">
