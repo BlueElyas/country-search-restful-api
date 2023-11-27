156b46362ca8c27a80f0d9421b494f205a533e21
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-27 14:18:25 +0000

    add styles to the header component

--- country-rest/src/Components/Header.jsx
+++ country-rest/src/Components/Header.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function Header() {
     return(
         <>
-            <h1 className="">Where in the world?</h1>
-            <button>Dark Mode</button>
+            <div className="bg-gray-600 text-white flex justify-between py-4 px-8">
+                <h1 className="font-bold text-xl">Where in the world?</h1>
+                <button>Dark Mode</button>
+            </div>
         </>
     )
 }
