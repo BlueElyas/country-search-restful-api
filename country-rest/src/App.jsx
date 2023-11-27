21a2ba2990e722b5b441caa7fb48007cc95c865c
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-27 14:19:39 +0000

    import countrycards component to app

--- country-rest/src/App.jsx
+++ country-rest/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import Header from './Components/Header'
-import data from '../data.json/'
+import CountryCards from './Components/CountryCards'
 
 function App() {
   return (
     
     <>
       <Header/>
+      <CountryCards/>
     </>
   )
 }
