78a9ae0899e667daa1cf765ef5d5aee62a70af5f
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-27 14:10:54 +0000

    add data from data.json file

--- country-rest/src/App.jsx
+++ country-rest/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import Header from './Components/Header'
+import data from '../data.json/'
 
 function App() {
-
   return (
+    
     <>
      <h1 className="text-3xl font-bold underline">
         Hello world!
