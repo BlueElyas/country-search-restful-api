448ac2d68b00ec6fd3c625fb52dcf6d98dec1dfc
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-28 23:53:27 +0000

    add responsive design and structure to countrypage component

--- src/Components/CountryPage.jsx
+++ src/Components/CountryPage.jsx
@@ -27,8 +27,10 @@ export default function CountryPage( { country, onBack, data, changeCountry } ) 
     return(
         <>
             <div className="text-gray-200 flex flex-col justify-start items-start px-16 h-128">
-                <button className="mt-16 shadow-md bg-gray-700 py-1 px-4 hover:scale-125 hover:bg-gray-300 hover:text-gray-700" onClick={onBack}> Go Back</button>
-                <div className="flex justify-center items-center gap-16 mt-8 md:flex-col">
+                <button 
+                className="mt-16 shadow-md bg-gray-700 py-1 px-4 hover:scale-125 hover:bg-gray-300 hover:text-gray-700 " 
+                onClick={onBack}> Go Back</button>
+                <div className="flex justify-center items-center gap-16 mt-8 flex-col sm:flex-row">
                     <img src={country.flags.png} alt={`${country.name} flag`}/>
                     <div className="">
                         <h1 className="text-2xl font-bold mb-4">{country.name}</h1>
