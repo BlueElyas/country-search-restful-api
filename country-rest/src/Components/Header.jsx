dc1c84ec6bd2e6b853267dd895bfb13cc1e12148
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-28 19:13:12 +0000

    add hover effects to header

--- country-rest/src/Components/Header.jsx
+++ country-rest/src/Components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <>
             <div className="bg-gray-700 text-white flex justify-between py-4 px-8">
                 <h1 className="font-bold text-xl">Where in the world?</h1>
-                <button>Dark Mode</button>
+                <button className="hover:scale-110 hover:bg-gray-300 hover:text-gray-700 px-4 py-1 rounded-sm">Dark Mode</button>
             </div>
         </>
     )
