80b71d5033eded0786d0fbf4fadbe99dfc9f4131
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-29 14:02:08 +0000

    add dark/light basics to project

--- src/Components/CountryCards.jsx
+++ src/Components/CountryCards.jsx
@@ -25,11 +25,11 @@ export default function CountryCards() {
 
     return(
         <>
-        <div className="bg-gray-800 min-h-[85%]">
+        <div className="bg-gray-100 dark:bg-gray-800 min-h-[85%]">
             <div className="relative px-4 pt-4 pb-2 flex justify-between gap-4 sm:px-12 flex-col items-start sm:flex-row ">
                 {renderInput ? <div className=""> 
                 <svg className="w-8 h-8 absolute bottom-7 left-16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
                 <input 
                     type="text" 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Header( { handleThemeChange } ) {
     return(
         <>
-            <div className="bg-gray-700 text-white flex justify-between py-4 px-8">
+            <div className="bg-white dark:bg-gray-700 text-white flex justify-between py-4 px-8">
                 <h1 className="font-bold text-xl">Where in the world?</h1>
                 <div className="flex items-center justify-center">
                     <svg className="w-6 h-6" viewBox="0 0 24 24" fill="all" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg> 
                     <button 
                         className="hover:scale-110 hover:bg-gray-300 hover:text-gray-700 px-4 py-1 rounded-sm"
