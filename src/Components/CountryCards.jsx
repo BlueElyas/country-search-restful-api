15a80dff4cc0b198054f542b9abd870111205550
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-29 17:31:47 +0000

    fixes structure in the country cards component

--- src/Components/CountryCards.jsx
+++ src/Components/CountryCards.jsx
@@ -44,7 +44,6 @@ export default function CountryCards() {
             })))
     },[])
 
-    console.log(countryData)
 
     return(
         <>
@@ -52,8 +51,17 @@ export default function CountryCards() {
             <div className=" px-4 pt-4 pb-2 flex justify-between gap-4 sm:px-12 flex-col items-start sm:flex-row mx-8 sm:mx-0">
                 {renderInput ? 
                 <div className="relative"> 
-                    <svg className="w-8 h-8 absolute top-3 left-2 sm:bottom-5 sm:left-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                    <svg 
+                        className="w-8 h-8 absolute top-3 left-2 sm:bottom-5 sm:left-5" 
+                        viewBox="0 0 24 24" 
+                        fill="none" 
+                        xmlns="http://www.w3.org/2000/svg">
+                    <path 
+                        d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" 
+                        stroke="#000000" 
+                        strokeWidth="2" 
+                        strokeLinecap="round" 
+                        strokeLinejoin="round"/>
                     </svg>
                     <input 
                         type="text" 
